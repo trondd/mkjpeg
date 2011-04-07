d2442c8d247f9d8558bd505e0964ea6952912574
mikel262 <mikel262@dee931bc-0e15-4f61-9269-4e18cfc4c3da>
2011-04-07 07:42:14 +0000

    memory initialization updated
    git-svn-id: http://opencores.org/ocsvn/mkjpeg/mkjpeg/trunk@66 dee931bc-0e15-4f61-9269-4e18cfc4c3da

--- design/JFIFGen/HeaderRAM.vhd
+++ design/JFIFGen/HeaderRAM.vhd
@@ -1,35 +1,56 @@
--- HeaderRam.vhd Khaleghian 8 Nov 2010
+LIBRARY ieee, std;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+use ieee.std_logic_textio.all;
+use std.textio.all;
 
-library ieee;
-library work;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
-use work.all;
-entity HeaderRam is
-port (
-d : in STD_LOGIC_VECTOR(7 downto 0);
-waddr : in STD_LOGIC_VECTOR(9 downto 0);
-raddr : in STD_LOGIC_VECTOR(9 downto 0);
-we : in STD_LOGIC;
-clk : in STD_LOGIC;
-q : out STD_LOGIC_VECTOR(7 downto 0)
-);
-end HeaderRam;
+ENTITY HeaderRam IS
+	GENERIC
+	(
+		ADDRESS_WIDTH	: integer := 10;
+		DATA_WIDTH		: integer := 8
+	);
+	PORT
+	(
+		clk				: IN  std_logic;
+		d				: IN  std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+		waddr			: IN  std_logic_vector(ADDRESS_WIDTH - 1 DOWNTO 0);
+		raddr			: IN  std_logic_vector(ADDRESS_WIDTH - 1 DOWNTO 0);
+		we				: IN  std_logic;
+		q				: OUT std_logic_vector(DATA_WIDTH - 1 DOWNTO 0)
+	);
+END HeaderRam;
 
-architecture syn of HeaderRam is
-type ram_type is array (1023 downto 0) of std_logic_vector (7 downto 0);
-signal RAM : ram_type;
-signal read_addr: STD_LOGIC_VECTOR(9 downto 0);
+ARCHITECTURE rtl OF HeaderRam IS
+
+TYPE RamType IS ARRAY(0 TO 2 ** ADDRESS_WIDTH - 1) OF std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+
+impure function InitRamFromFile(RamFileName : in string) return RamType is
+	FILE RamFile : text is in RamFileName;
+	variable RamFileLine : line;
+	variable RAM : RamType;
 begin
-q <= RAM(conv_integer(read_addr)) ;
-process (clk)
-begin
-if clk'event and clk = '1'
-then
-if we='1' then
-RAM(conv_integer(waddr)) <= d;
-end if;
-read_addr <= raddr;
-end if;
-end process;
-end syn; 
+	for l in RamType'range loop
+		readline(RamFile, RamFileLine);
+		hread(RamFileLine, RAM(l));
+	end loop;
+	return RAM;
+end function;
+
+--SIGNAL ram_block : RamType := InitRamFromFile("../design/jfifgen/header.hex");
+SIGNAL ram_block : RamType;
+attribute ram_init_file : string;
+attribute ram_init_file of ram_block :
+signal is "./src/jpg/JFIFGen/header.mif";
+BEGIN
+	PROCESS (clk)
+	BEGIN
+		IF (clk'event AND clk = '1') THEN
+			IF (we = '1') THEN
+			    ram_block(to_integer(unsigned(waddr))) <= d;
+			END IF;
+
+			q <= ram_block(to_integer(unsigned(raddr)));
+		END IF;
+	END PROCESS;
+END rtl;
